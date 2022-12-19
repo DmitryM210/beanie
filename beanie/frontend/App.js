4e1b421bfd03dd6950bca54f2d0bd7605af201fa
Artem Suvorov <suvorov.artem@hotmail.com>
2022-12-19 14:43:50 +0500

    fix home page not found bug

--- beanie/frontend/App.js
+++ beanie/frontend/App.js
@@ -5,11 +5,11 @@ import Footer from './Footer/Footer';
 import HomePage from './HomePage/HomePage';
 import Environment from './Environment/Environment';
 
-const levelsByUrl = {
-  "/": 0,
-  "/level/1/": 1,
-  "/level/2/": 2
-};
+// const levelsByUrl = {
+//   "/": 0,
+//   "/level/1/": 1,
+//   "/level/2/": 2
+// };
 
 export class App extends Component {
   constructor(props) {
@@ -21,7 +21,9 @@ export class App extends Component {
 
   componentDidMount() {
     // console.log(window.location);
-    const level = levelsByUrl[window.location.pathname];
+    const pattern = /\/level\/(?<level>[0-9]+)\//g
+    const [match] = window.location.href.matchAll(pattern);
+    const level = match?.groups?.level || 0;
     this.setState({ selectedLevel: level });
   }
 
